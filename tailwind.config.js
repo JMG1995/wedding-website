49987194aea785dae253d9d6574714e1b9ae2730
Joseph <jmgrant1995@gmail.com>
2020-06-28 14:33:56 +0800

    Remove some unused files and build out hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: [
-    './src/**/*.js',
-  ],
-  theme: {},
-  variants: {},
-  // https://github.com/tailwindcss/custom-forms
-  plugins: [require("@tailwindcss/custom-forms")],
+	purge: ['./src/**/*.js'],
+	theme: {
+		maxHeight: {
+			'600': '600px',
+		},
+		fontFamily: {
+			'permanent-marker': ['Permanent Marker'],
+		},
+	},
+	variants: {},
+	// https://github.com/tailwindcss/custom-forms
+	plugins: [require('@tailwindcss/custom-forms')],
 };
