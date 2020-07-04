58a782f47a4566a1c15b369c7410604596370665
Joseph <jmgrant1995@gmail.com>
2020-07-04 15:37:13 +0800

    Couple of changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Starter Tailwind`,
-		description: `Gatsby starter styled with Tailwind`,
-		author: `@taylorbryant`,
+		title: `Anthea + Joseph Wedding`,
+		description: `RSVP to Anthea and Joseph's Wedding`,
+		author: `@josephgrant`,
 	},
 	plugins: [
 		`gatsby-plugin-eslint`,
@@ -15,13 +15,12 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-tailwind`,
+				name: `anthea-joseph-wedding`,
 				short_name: `starter`,
 				start_url: `/`,
 				background_color: fullConfig.theme.colors.white,
 				theme_color: fullConfig.theme.colors.teal['400'],
 				display: `minimal-ui`,
-				icon: `src/images/hero.jpg`,
 			},
 		},
 		{
